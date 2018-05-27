4ce18153368bbd482d1b2cabd18f32e19645fa09
Milos Tepic <milos837@gmail.com>
2018-05-27 22:22:39 +0200

    added regexp pattern for user attributes

--- src/main/java/com/example/final_project_test/entities/dto/TeacherDto.java
+++ src/main/java/com/example/final_project_test/entities/dto/TeacherDto.java
@@ -1,6 +1,7 @@
 package com.example.final_project_test.entities.dto;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 import com.example.final_project_test.validation.UniqueUsername;
@@ -10,21 +11,26 @@ public class TeacherDto {
 	@UniqueUsername
 	@NotNull(message = "Username must not be null")
 	@Size(min = 5, max = 15, message = "Username must be between {min} and {max} characters.")
+	@Pattern(regexp = "^[a-zA-Z0-9]*$", message = "Username can contain only alphanumeric characters.")
 	private String username;
 	
 	@NotNull(message = "Password must not be null")
 	@Size(min = 5, max = 15, message = "Password must be between {min} and {max} characters.")
+	@Pattern(regexp = "^[a-zA-Z0-9]*$", message = "Password can contain only alphanumeric characters.")
 	private String password;
 	
-	@NotNull(message = "Confirm password must not be null.")
+	@NotNull(message = "Confirmed password must not be null.")
+	@Pattern(regexp = "^[a-zA-Z0-9]*$", message = "Password can contain only alphanumeric characters.")
 	private String confirmedPassword;
 	
 	@NotNull(message = "First name must not be null.")
 	@Size(min = 2, max = 30, message = "First name must be between {min} and {max} characters.")
+	@Pattern(regexp = "^[a-zA-Z]*$", message = "First name can contain only uppercase and lowercase letters.")
 	private String firstName;
 	
 	@NotNull(message = "Last name must not be null")
 	@Size(min = 2, max = 30, message = "First name must be between {min} and {max} characters.")
+	@Pattern(regexp = "^[a-zA-Z]*$", message = "Last name can contain only uppercase and lowercase letters.")
 	private String lastName;
 
 	public TeacherDto() {
